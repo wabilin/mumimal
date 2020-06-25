8bb57a125a5f4d6c6c1d9beeb392b5cf4f7a644b
wabilin <twelendil@protonmail.com>
2020-06-25 22:14:55 +0900

    log meta and html

--- src/parsePosts.js
+++ src/parsePosts.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const marked = require('marked')
+const matter = require('gray-matter');
 
 const { readDir } = require('./file')
 const { POSTS_PATH } = require('./paths')
@@ -5,7 +8,14 @@ const { POSTS_PATH } = require('./paths')
 async function parsePosts() {
   const filenames = await readDir(POSTS_PATH)
 
-  filenames.filter(name => name.match(/.md$/)).forEach(x => console.log(x))
+  const postNames = filenames.filter(name => name.match(/.md$/))
+  const postPaths = postNames.map(name => path.join(POSTS_PATH, name))
+
+  postPaths.forEach(filePath => {
+    const { content, data } = matter.read(filePath)
+    console.log(JSON.stringify(data))
+    console.log(marked(content))
+  })
 }
 
 module.exports = {
